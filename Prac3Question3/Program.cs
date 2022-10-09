ec2c0a69361e0f88c81510fc02e7a70c3e5d5cc5
wandilechauke <115423208+wandilechauke@users.noreply.github.com>
2022-10-09 20:17:21 +0200

    Add files via upload

---
+++ Prac3Question3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac3Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Variables
+            int plotLength;
+            int plotWidth, houseLength, houseWidth, plotArea, houseArea, grassArea;
+
+            // Accept user input
+            Console.Write("Please type in length of plot: ");
+            plotLength = int.Parse(Console.ReadLine());
+            Console.Write("Please type in width of plot: ");
+            plotWidth = int.Parse(Console.ReadLine());
+            Console.Write("Please type in length of house: ");
+            houseLength = int.Parse(Console.ReadLine());
+            Console.Write("Please type in width of house: ");
+            houseWidth = int.Parse(Console.ReadLine());
+
+            // Calculation
+            plotArea = plotLength * plotWidth;
+            houseArea = houseLength * houseWidth;
+            grassArea = plotArea - houseArea;
+
+            //Output
+            Console.WriteLine(); //Blank line
+            Console.WriteLine("Area to be mowed:{0}",grassArea);
+
+            Console.ReadLine();
+        }
+    }
+}
